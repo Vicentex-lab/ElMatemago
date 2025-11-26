b10252c9084039ac0de95a96478a9a6aee41c32b
Nickolaus20 <nicolasignacio.acuna@alumnos.ulagos.cl>
2025-11-25 21:16:57 -0300

    Escudo y main actualizado
    lo cambia todo. D:

--- Menu/items.py
+++ Menu/items.py
@@ -1,13 +1,17 @@
 class item():
-    def __init__(self, places, name):
+    def __init__(self, places, name, damage):
         self.places=places
         self.name=name
+        self.damage=damage
         
 class sword(item):
     name="Espada Divisora"
-    places=[[18][2], [16][3], [2][7], [2][11], [17][26], [23][18]] #0, 1, 2, 3, 4, 5
+    places_y=[3, 2, 7, 11, 26, 23] #0, 1, 2, 3, 4, 5
+    places_x=[16, 18, 2, 2, 17, 18]
+    damage=3
     
-    
-#hola
-#lolsuperman
-#chao
+class shield(item):
+    name="Escudo Absoluto"
+    places_y=[ 1, 1, 1, 1, 1, 1] 
+    places_x=[ 1, 1, 1, 1, 1, 1]
+    damage=0
