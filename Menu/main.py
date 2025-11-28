33d0593aba7b1a012d3933a81f147db27b88e2f3
Karibetz <204908485+Karibetz@users.noreply.github.com>
2025-11-27 23:27:35 -0300

    Nuevo record de puntaje 1

--- Menu/main.py
+++ Menu/main.py
@@ -485,7 +485,10 @@ def jugar():
             running = False
             print("Puntaje sin bonus por tiempo:", player_pts)
             print("Segundos", temporizador/60)
-            if temporizador/60<=15:
+            if temporizador/60<=12:
+                player_pts+=2000
+                print("TIEMPO INHUMANO") 
+            elif temporizador/60<=15:
                 player_pts+=1000
             elif temporizador/60<=20:
                 player_pts+=500
