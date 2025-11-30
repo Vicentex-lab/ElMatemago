f2b1d79a909387f1bf6f0f5f26797353d0e37169
Vice980 <vicentetapiam980@gmail.com>
2025-11-30 00:56:01 -0300

    Add files via upload

---
+++ Menu/sprites.py
@@ -0,0 +1,16 @@
+import pygame
+import configuracion as cfg
+
+#Hacemos funcion para cargar sprites
+
+def load(name):
+    path = f"./assets/{name}.png"
+    img = pygame.image.load(path).convert_alpha()
+    return pygame.transform.scale(img, (cfg.TILE, cfg.TILE))
+
+MAGO = load("MAGO")
+CERO = load("CERO")
+RAIZ = load("RAIZNEGATIVA")
+PIGARTO = load("PIGARTO")
+ESPADA = load("ESPADA")
+ESCUDO = load("ESCUDO")
